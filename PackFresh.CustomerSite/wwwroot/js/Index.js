f13d072e0cd2485399794471eaca6de544754da3
anuragsil <silwal.anurag@gmail.com>
2018-06-21 13:10:50 +1200

    Home page next previous for gallery and review

---
+++ PackFresh.CustomerSite/wwwroot/js/Index.js
@@ -0,0 +1,83 @@
+﻿//Start This is for the background slider
+
+var myIndex = 0;
+carousel();
+
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("bg-slides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) { myIndex = 1 }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 3000); // Change image every 2 seconds
+}
+
+//End This is for the background slider
+
+
+
+
+
+//Script for the gallery next and previous
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("gallery-slide");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+
+//End Script for the gallery next and previous
+
+
+
+//This is the script for the bottom customer review.
+
+var slideIndex = 1;
+showReviewSlides(slideIndex);
+
+function plusReviewSlides(n) {
+    showReviewSlides(slideIndex += n);
+}
+
+
+function showReviewSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("cus-review");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    slides[slideIndex - 1].style.display = "block";
+
+}
+
+
+//End This is the script for the bottom customer review.
