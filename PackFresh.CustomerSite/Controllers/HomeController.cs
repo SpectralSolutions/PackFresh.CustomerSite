9be2ad78632fa5c6f11fa348ec43b181eb0ace79
anuragsil <silwal.anurag@gmail.com>
2018-09-03 15:05:24 +1200

    Contact to send email

--- PackFresh.CustomerSite/Controllers/HomeController.cs
+++ PackFresh.CustomerSite/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PackFresh.CustomerSite.Models;
+using System.Net.Mail;
 
 namespace PackFresh.CustomerSite.Controllers
 {
@@ -39,6 +40,47 @@ namespace PackFresh.CustomerSite.Controllers
             ViewData["Message"] = "Your contact page.";
 
             return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Contact(Contact model)
+        {
+
+            try
+            {
+                Contact contact = new ContactViewModelFactory().generateContactFromCreateContactViewModel(model);
+
+                MailMessage msz = new MailMessage();
+                msz.From = new MailAddress(contact.Email); 
+
+                msz.To.Add("silwal.anurag@gmail.com"); //Where mail will be sent (Pack Fresh Customer Team)
+                msz.Subject = contact.Subject;
+                msz.Body = "From: " + contact.Name + " Company: " + contact.Company + "Phone: " + contact.Phone + " Message: " + contact.Message;
+                SmtpClient smtp = new SmtpClient();
+
+                smtp.Host = "smtp.gmail.com";
+
+                smtp.Port = 587;
+
+                smtp.Credentials = new System.Net.NetworkCredential
+                ("teamauk124@gmail.com", "CPPTeam124");
+
+                smtp.EnableSsl = true;
+
+                smtp.Send(msz);
+
+                ModelState.Clear();
+                ViewBag.Message = "Thank you for Contacting us ";
+            }
+            catch (Exception ex)
+            {
+                ModelState.Clear();
+                ViewBag.Message = $" Sorry we are facing Problem here {ex.Message}";
+            }
+
+
+            return RedirectToAction("Index");
         }
 
         public IActionResult Error()
---
+++ PackFresh.CustomerSite/Models/Contact.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PackFresh.CustomerSite.Models
+{
+    public class Contact
+    {
+        [Required]
+        [StringLength(20, MinimumLength = 5)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Phone { get; set; }
+
+        public string Company { get; set; }
+
+        [Required]
+        public string Subject { get; set; }
+        
+        [Required]
+        public string Message { get; set; }
+    }
+}
